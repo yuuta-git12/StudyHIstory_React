9ec6340449f71206c3c20b24b1789fc993fde41c
yuuta-git12 <tayu2201@icloud.com>
2024-12-22 23:32:31 +0900

    [add]Supabaseのstudy-recordテーブルからデータを読み込むコンポーネント作成

---
+++ src/components/StudyRecords.jsx
@@ -0,0 +1,49 @@
+import React,{ useEffect, useState} from "react";
+import { supabase } from "../supabaseClient";
+
+export const StudyRecords = () => {
+    const [studyRecords, setStudyRecords] = useState([]);
+    const [loading, setLoading] = useState();
+
+    useEffect(() => {
+        const fectchUsers = async() =>{
+            try{
+                const { data, error } = await supabase
+                .from('study-record')
+                .select('*');
+
+                if(error){
+                    console.error('Error fetching users:',error);
+                }else{
+                    setStudyRecords(data);
+                }
+
+            }catch(error){
+                console.error('Unexpected error:',error);
+            }finally{
+                setLoading(false);
+            }
+        };
+        fectchUsers();
+    },[]);
+    if (loading) return <p>Loading....</p>;
+
+    return(
+        <>
+        <div className="card">
+        <ul>
+          {studyRecords.map((record) => (
+            <li key={record.id}>
+              <p>
+                タイトル：{record.title} 学習時間:{record.time}時間
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p>合計学習時間:時間</p>
+      </div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StudyRecords} from './components/StudyRecords.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <StudyRecords/>
   </StrictMode>,
 )
