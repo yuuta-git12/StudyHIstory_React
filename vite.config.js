18e89155dee7ef969cc67698b871ea4246a67654
yuuta-git12 <tayu2201@icloud.com>
2024-12-22 23:31:49 +0900

    [add]env読み込み用設定ファイル追加

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import env from "vite-plugin-env-compatible"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
 })
